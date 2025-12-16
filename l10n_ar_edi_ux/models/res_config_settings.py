909ac973226e66d3c01b16733c10d016b21c286a
LP <openerp@elepeservicios.com.ar>
2025-12-16 16:01:12 -0300

    [MOD] modules moved to odoo_argentina

--- l10n_ar_edi_ux/models/res_config_settings.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = "res.config.settings"
-
-    arba_cit = fields.Char(related="company_id.arba_cit", readonly=False)
--- l10n_ar_edi_ux/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import test_padron_afip
