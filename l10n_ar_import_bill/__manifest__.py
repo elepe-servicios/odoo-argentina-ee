c58ae593e6e49f1f71820407280783535bbb4158
LP <openerp@elepeservicios.com.ar>
2026-02-14 21:32:08 -0300

    make modules uninstallable

--- l10n_ar_import_bill/__manifest__.py
+++ l10n_ar_import_bill/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Argentinian Importing Bills from ARCA",
-    "version": "19.0.1.0.0",
+    "version": "19.0.1.0.1",
     "category": "Localization/Argentina",
     "sequence": 8,
     "author": "ADHOC SA",
@@ -13,7 +13,7 @@
         "wizards/afip_import_wizard.xml",
         "views/account_move.xml",
     ],
-    "installable": True,
+    "installable": False,
     "auto_install": False,
     "application": False,
 }
