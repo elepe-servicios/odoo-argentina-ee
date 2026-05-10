aa9dd41d6db30fe69986f13bc0fd38e284063edb
Leonardo Dibarbora <leodibarbora@gmail.com>
2026-05-10 14:38:12 -0300

    [Mig] Disable account reports extensions

--- account_accountant_ux/__manifest__.py
+++ account_accountant_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Accounting Accountant UX",
-    "version": "18.0.1.8.0",
+    "version": "18.0.1.8.1",
     "category": "Accounting",
     "sequence": 14,
     "summary": "",
@@ -36,7 +36,7 @@
         "views/res_partner_view.xml",
         "views/account_move_line.xml",
         "views/bank_rec_widget_views.xml",
-        "views/account_report_view.xml",
+#        "views/account_report_view.xml",
         "wizards/account_change_lock_date_views.xml",
         "wizards/res_config_settings_views.xml",
         "wizards/account_reconcile_wizard.xml",
--- account_accountant_ux/models/__init__.py
+++ account_accountant_ux/models/__init__.py
@@ -8,4 +8,4 @@ from . import account_move
 from . import res_partner
 from . import account_journal_dashboard
 from . import account_partner_ledger
-from . import account_report
+# from . import account_report disabled because field require_custom_filter already exists in account_report module of account_reports, and it creates a conflict when both modules are installed. The field should be moved to account_report module of account_reports to avoid this conflict and then this import can be re-enabled.
